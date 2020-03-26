cbbfdc295ffa599ff4d4ddfe1c4f2fadb249fa62
Jesse Schulman <jesse@caleos.io>
2020-03-26 11:45:33 -0700

    Don't log pk

--- libs/eosio-lib.js
+++ libs/eosio-lib.js
@@ -13,7 +13,6 @@ export async function create(accountName, ownerKey, activeKey) {
 
   const signatureProvider = new JsSignatureProvider([pk]);
   console.log(process.env.eosioApiEndPoint);
-  console.log(pk);
 
   const rpc = new JsonRpc(process.env.eosioApiEndPoint, { fetch });
 
