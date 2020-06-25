8eed609e8b329c521df2d6d5894bfc33b1ad843b
Jesse Schulman <jesse@caleos.io>
2020-06-24 23:25:21 -0700

    Fix env var issue

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,19 +23,33 @@ const envDev = {
   SERVER_PORT: 3000
 }
 
-const envProd = Object.assign({}, envDev, {
+const envProd = {
   NODE_ENV: 'production',
-  accountCreatorKey: 'tf-account-creator-key',
-  allowDeleteNumber: 'N',
-  eosioApiEndPoint: 'https://telos.caleos.io',
   tableName: 'prod-accounts',
   testnetRotationTableName: 'prod-testnet-rotation',
   testnetFaucetTableName: 'prod-testnet-faucet',
+  accountCreatorAccount: 'accounts.tf',
+  accountCreatorContract: 'accounts.tf',
+  accountCreatorKey: 'tf-account-creator-key',
+  allowDeleteNumber: 'N',
+  eosioApiEndPoint: 'https://telos.caleos.io',
+  sentryDsn: 'https://4fb0b518dbf74512a27bf8bb24977136@sentry.io/1749694',
+  testnetApiEndPoint: 'https://testnet.telos.caleos.io',
+  testnetAutorotateAccount: 'autorotate',
+  testnetAutorotateKey: 'testnet-autorotate-key',
+  testnetFaucetAccount: 'faucet.tf',
+  testnetFaucetKey: 'testnet-faucet-key',
+  twilioAccountSid: 'tf-twilio-sid',
+  twilioAuthToken: 'tf-twilio-auth',
+  twilioSmsNumber: '+18178356742',
+  tableName: 'dev-accounts',
+  testnetRotationTableName: 'dev-testnet-rotation',
+  testnetFaucetTableName: 'dev-testnet-faucet',
   API_LOG_LEVEL: 'debug',
   MODE: 'prod',
   SERVER_HOSTNAME: 'api.telos.net',
   SERVER_PORT: 4000
-})
+}
 
 const dev = {
   name: 'api-dev',
