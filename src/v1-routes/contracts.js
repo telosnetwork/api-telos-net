c9cd0a0e66071df41ec0aba1fe2a54e27a679b17
DonaldPeat <don@caleos.io>
2022-01-03 19:29:58 -0600

    refactor: consolidate into one api method, add code prop, remove unnec data prop migrating

--- src/v1-routes/contracts.js
+++ src/v1-routes/contracts.js
@@ -1,25 +1,19 @@
 const verificationLib = require("../libs/verification-lib");
 
 const parseMultiForm = (request, done) => {
-    const fileList = Object.keys(request.files);
-    request.body.files = [];
-    for (let file of fileList){
-        const testFileData = request.files[file].data;
-        const contractString = testFileData.toString('utf8');
-        console.log(contractString);
-        request.files[file]['string'] = contractString;
-        request.body.files.push(request.files[file])
+    const files = request.files;
+    for (let i in files){
+        files[i]['code'] = files[i].data.toString('utf8');
     }
-    delete request.files; 
     done();  
 };
 
 const verificationOpts = {
     schema: {
         summary: 'verifies source code for solidity contract',
-        tags: ['contracts'],
+        tags: ['evm'],
         body: {
-            required: ['contractAddress','fileName','compilerVersion', 'contractCode'],
+            required: ['compilerVersion'],
             type: 'object',
             properties: {
                 contractAddress: {
@@ -27,31 +21,26 @@ const verificationOpts = {
                     type: 'string',
                     example: '0xc4c89dD46524c6f704e92a9Cd012a3EbaDAdFF36'
                 },
-                fileName: {
-                    description: 'name of .sol file',
-                    type: 'string',
-                    example: 'HelloWorld.sol'
-                },
                 compilerVersion: {
                     description: "compiler version. see https://github.com/ethereum/solc-bin/blob/gh-pages/bin/list.json",
                     type: 'string',
                     example: 'v0.4.23+commit.124ca40d'
                 },
-                contractCode: {
-                    description: "Raw string containing contract code",
-                    type: 'string',
-                    example: `/** *Submitted for verification at Etherscan.io on 2018-04-21*/ pragma solidity ^0.4.0; contract HelloWorld {address public owner;modifier onlyOwner() { require(msg.sender == owner); _; }constructor() public {owner = msg.sender;}function salutaAndonio() public pure returns(bytes32 hw){hw = "HelloWorld";}function killMe() public onlyOwner {selfdestruct(owner);}}`
-                },
-                optimized: {
-                    description: 'flag for optimization when compiling',
-                    type: 'boolean',
-                    example: false
-                },
-                runs: {
-                    description: 'Optimization value for frequency',
-                    type: 'number',
-                    example: 200
-                }
+                // files: {
+                //     description: "array of file objects containing code as string",
+                //     type: 'array',
+                //     example: `[{ name: 'test.sol', code: 'pragma solidity 0.8.7 ...}, { name: test2.sol ...} ...]` 
+                // },
+                // optimized: {
+                //     description: 'flag for optimization when compiling',
+                //     type: 'boolean',
+                //     example: false
+                // },
+                // runs: {
+                //     description: 'Optimization value for frequency',
+                //     type: 'number',
+                //     example: 200
+                // }
             }
         },
         response: {
@@ -69,18 +58,19 @@ const verificationOpts = {
 
 const verificationHandler = async(request, reply) => {
 
-    const contractAddress = request.body.contractAddress;
-    const fileName = request.body.fileName;
+    // const contractAddress = request.body.contractAddress;
     const compilerVersion = request.body.compilerVersion;
-    const contractCode = request.body.contractCode;
+    const contractCode = request.body.files[0].code;
 
-    if (!contractAddress ) {
-        return reply.code(400).send("Must specify deployed contract address");
-    }
-    
-    if (!fileName ) {
-        return reply.code(400).send("Must specify file name e.g. 'myContract.sol'");
-    }
+    // if (!contractAddress ) {
+    //     return reply.code(400).send("Must specify deployed contract address");
+    // }
+
+    // const isContractAddress = await verificationLib.isContract(contractAddress);
+
+    // if (!isContractAddress) {
+    //     return reply.code(400).send(`${contractAddress} is not a valid contract address`);
+    // }
 
     if (!compilerVersion ) {
         return reply.code(400).send("Must specify compiler version");
@@ -90,60 +80,54 @@ const verificationHandler = async(request, reply) => {
         return reply.code(400).send("No contract code submitted");
     }
 
-    const isContractAddress = await verificationLib.isContract(contractAddress);
-
-    if (!isContractAddress) {
-        return reply.code(400).send(`${contractAddress} is not a valid contract address`);
-    }
-
     const verificationStatus = await verificationLib.verifyContract(request.body);
     const message = verificationStatus ? 'Contract verified' : 'Verification failed';
     reply.send(message);
 }
 
-const fileUploadOpts = {
-    schema: {
-        tags: ['contracts'],
-        summary: 'upload solidity contract file(s)',
-        body: {
-            type: 'object',
-            properties: {
-                compilerVersion: { 
-                    type: 'string',
-                },
-                files: {
-                    type: 'array'
-                }
+// const fileUploadOpts = {
+//     schema: {
+//         tags: ['evm'],
+//         summary: 'upload solidity contract file(s)',
+//         body: {
+//             type: 'object',
+//             properties: {
+//                 compilerVersion: { 
+//                     type: 'string',
+//                 },
+//                 files: {
+//                     type: 'array'
+//                 }
 
-            }
-      }
-    },
-    response: {
-        200: {
-          description: 'Succesful upload',
-          type: 'object',
-          properties: {
-            path: {type: 'string'}
-          }
-        },
-        500: {
-          description: 'Error response',
-          type: 'object',
-          properties: {
-            error: {type: 'string'},
-            message: {type: 'string'},
-            statusCode: {type: 'number'}
-          }
-        }
-      }
-};
+//             }
+//       }
+//     },
+//     response: {
+//         200: {
+//           description: 'Succesful upload',
+//           type: 'object',
+//           properties: {
+//             path: {type: 'string'}
+//           }
+//         },
+//         500: {
+//           description: 'Error response',
+//           type: 'object',
+//           properties: {
+//             error: {type: 'string'},
+//             message: {type: 'string'},
+//             statusCode: {type: 'number'}
+//           }
+//         }
+//       }
+// };
 
-const fileUploadHandler = async (request, reply) => {
-    reply.send(200);
-  }
+// const fileUploadHandler = async (request, reply) => {
+//     reply.send(200);
+//   }
 
 module.exports = async (fastify, options) => {
     fastify.post('contracts/verify', verificationOpts, verificationHandler)
-    fastify.post('contracts/upload', fileUploadOpts, fileUploadHandler)
+    // fastify.post('contracts/upload', fileUploadOpts, fileUploadHandler)
     fastify.addContentTypeParser('multipart/form-data', parseMultiForm);
 }
