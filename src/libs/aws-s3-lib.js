c81299761b03d8826460f11919fe91d6498e79af
DonaldPeat <don@caleos.io>
2022-02-10 18:17:38 -0600

    refactor: consolidate aws files, remove deprecated verification endpoints & methods from contracts routes

--- src/libs/aws-s3-lib.js
+++ src/libs/aws-s3-lib.js
@@ -1,62 +1,53 @@
 const AWS = require("aws-sdk");
 const clientS3 = new AWS.S3();
 const Bucket = process.env.VERIFIED_CONTRACTS_BUCKET;
-const OUTPUT_FILENAME = 'output.json';
-const INPUT_FILENAME = 'input.json';
-const ABI_FILENAME = 'abi.json';
+export const SOURCE_FILENAME = 'source.json';
+export const METADATA_FILENAME = 'metadata.json';
 
 async function isVerified(contractAddress){
     let headInfo;
     const params = { Bucket , Key: `${contractAddress}/${OUTPUT_FILENAME}` };
     try{
-        headInfo = await clientS3.headObject(params).promise();  
+        await clientS3.headObject(params).promise();  
+        return true;
     }catch(e){
         //aws returns 404 if key isn't found
-        return { status: 404, message: 'contract has not been verified' };
+        return false; 
     }
     return { status: true, message: headInfo.LastModified};    
 }
 
-async function getOutput(contractAddress){
-    const params = { Bucket , Key: `${contractAddress}/${OUTPUT_FILENAME}` };
+export async function getMetadata(contractAddress){
+    const params = { Bucket , Key: `${contractAddress}/${METADATA_FILENAME}` };
     try{
         return await clientS3.getObject(params).promise();
     }catch(e){
         return { status: 404, message: 'file not found'}
     }
-}
-
-async function getInput(contractAddress){
-    const params = { Bucket , Key: `${contractAddress}/${INPUT_FILENAME}` };
+  }
+  
+  export async function getSource(contractAddress){
+    const params = { Bucket , Key: `${contractAddress}/${SOURCE_FILENAME}` };
     try{
         return await clientS3.getObject(params).promise();
     }catch(e){
         return { status: 404, message: 'file not found'}
     }
-}
-
-async function getAbi(contractAddress){
-    const params = { Bucket , Key: `${contractAddress}/${ABI_FILENAME}` };
-    try{
-        return await clientS3.getObject(params).promise();
-    }catch(e){
-        return { status: 404, message: 'file not found'}
-    }
-}
-
-async function uploadObject(contractAddress, buffer, contentType){
+  }
+  
+  export async function uploadObject(contractAddress, buffer){
     const params = { 
         Bucket , 
         Key: contractAddress, 
         Body: buffer, 
         ACL: 'public-read', 
-        ContentType: contentType
+        ContentType: 'application/json'
     };
     try{
         return await clientS3.putObject(params).promise();
     }catch(e){
         return e;
     }
-}
+  }
 
-module.exports = { isVerified, uploadObject, getOutput, getInput, getAbi }
+module.exports = { isVerified, getMetadata, getSource, uploadObject }
