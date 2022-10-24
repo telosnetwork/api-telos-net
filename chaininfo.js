6730d2b1fce0cddaaae6f61e3233d9d34059c055
Don Peat <don@caleos.io>
2022-10-24 03:06:23 -0500

    fix: migrate apy methods to chain routes

--- chaininfo.js
+++ chaininfo.js
@@ -1,9 +1,6 @@
 import { getCurrencyStats, getCurrencyBalance, getRexStats } from "./libs/eosio-lib";
 import { respond } from './libs/response-lib';
 import { exclude } from "./src/utils/exclude";
-import { getTableRows } from "./src/libs/eosio-lib";
-import Big from 'big.js';
-import { BigNumber, ethers } from 'ethers';
 
 const cmcCirculationExclusions = ["exrsrv.tf", "tf", "eosio.saving", "free.tf", "eosio.names",
     "econdevfunds", "eosio.ram", "ramadmin.tf", "ramlaunch.tf", "treasury.tf", "accounts.tf", "grants.tf"];
@@ -42,123 +39,6 @@ export async function totalStaked(event, context) {
 //     const totalLendable = parseFloat(rex.total_lendable);
 //     return respond(200, ((1.7e6 * 12 / totalLendable) * 100).toFixed(2));
 // }
-
-/**
- * Calculates and returns the current APY (annual percentage yield) for sTLOS
- *
- * @param   {string} tvl      - total volume locked in wei, as a string
- * @returns {Promise<string>} - calculated APY as a unitless number, eg. "33.25"
- */
- export async function fetchStlosApy(tvl) {
-    const apyStats = await getApyStats(tvl);
-
-    if (!apyStats){
-        return;
-    }
-
-    if (apyStats.balanceRatio === 0){
-        return apyStats.balanceRatio.toString();
-    }
-
-    const stlosPayout = annualPayout.minus(rexPayout);
-    const apy = stlosPayout.div(stlosTotal).times(100).toFixed(2);
-
-    return apy;
-}
-
-/**
- * Calculates and returns the current APY (annual percentage yield) for native
- *
- * @param   {string} tvl      - total volume locked in wei, as a string
- * @returns {Promise<string>} - calculated APY as a unitless number, eg. "33.25"
- */
- export async function fetchNativeApy(tvl) {
-    let apyStats;
-
-    try {
-        apyStats = await getApyStats(tvl);
-    }catch(e){
-        console.error(e);
-        return;
-    }
-
-    if (apyStats.balanceRatio === 0){
-        return apyStats.balanceRatio.toString();
-    }
-
-    const rexPayout = apyStats.annualPayout.div(apyStats.balanceRatio.plus(1));
-    const apy = rexPayout.div(rexTotal).times(100).toFixed(2);
-
-    return apy;
-}
-
-/**
- * Calculates and returns the current APY (annual percentage yield) for native
- *
- * @param   {string} tvl      - total volume locked in wei, as a string
- * @returns {Promise<{balanceRatio: number, annualPayout: number}>} - calculated APY as a unitless number, eg. "33.25"
- */
-
-async function getApyStats(tvl) {
-    const tvlBn = BigNumber.from(tvl);
-    const zeroBal = {balanceRatio: 0, annualPayout: 0};
-
-    if (tvlBn.eq('0')) {
-        return zeroBal;
-    }
-
-    const rexPoolResponse = await getTableRows({
-        code: 'eosio',
-        scope: 'eosio',
-        table: 'rexpool',
-        limit: '1',
-    });
-
-    if (!rexPoolResponse || !rexPoolResponse.rows.length) {
-        throw 'Failed to fetch rexpool';
-    }
-
-    const distConfigResponse = await getTableRows({
-        code: 'exrsrv.tf',
-        scope: 'exrsrv.tf',
-        table: 'config',
-        limit: '1',
-    });
-
-    if (!distConfigResponse || !distConfigResponse.rows.length) {
-        throw 'Failed to fetch exrsrv.tf config';
-    }
-
-    const payoutsResponse = await getTableRows({
-        code: 'exrsrv.tf',
-        scope: 'exrsrv.tf',
-        table: 'payouts',
-        limit: '100',
-    });
-
-    if (!payoutsResponse || !payoutsResponse.rows.length) {
-        throw 'Failed to fetch exrsrv.tf payouts';
-    }
-
-    const rexStats = rexPoolResponse.rows[0];
-    const distConfig = distConfigResponse.rows[0];
-    const payoutRow = payoutsResponse.rows.find((row) => row.to === 'eosio.rex');
-
-    const annualPayout = new Big(payoutRow.amount).times(new Big(60 * 60 * 24 * 365).div(payoutRow.interval));
-    const fixedRatio = new Big(distConfig.ratio).div(100);
-    const rexTotal = new Big(rexStats.total_lendable.split(' ')[0]);
-    const stlosTotal = new Big(ethers.utils.formatEther(tvlBn));
-
-    const balanceRatio = rexTotal.eq(0) ? -1 : stlosTotal.times(fixedRatio).div(rexTotal.add(stlosTotal));
-
-    if (apyStats.balanceRatio.eq(0)) {
-        return zeroBal;
-    }
-
-    return  { balanceRatio, annualPayout };
-}
-
-
 
 export async function rexStaked(event, context) {
     const rex = await getRexStats();
