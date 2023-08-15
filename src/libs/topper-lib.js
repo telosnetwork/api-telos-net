f71d6def40984fc1a8a3b412126d5af311863ce6
donnyquixotic <don@caleos.io>
2023-08-14 23:19:28 -0500

    fix: fetch pk from aws secrets for widget bootstrap token creation

--- src/libs/topper-lib.js
+++ src/libs/topper-lib.js
@@ -1,16 +1,39 @@
-require("dotenv").config();
+const { getKeyBySecretName } = require("./auth-lib");
 const { createPrivateKey, randomUUID } = require('crypto');
 const { promisify } = require('util');
 const jsonwebtoken = require('jsonwebtoken');
 
-// Load private key in JWK format from an environment variable.
-const privateKeyJwk = JSON.parse(process.env.TOPPER_KEY);
-
 // Promisify the `jsonwebtoken.sign()` method for simplicity.
 const sign = promisify(jsonwebtoken.sign);
 
-// Parse the JWK formatted key.
-const privateKey = createPrivateKey({ format: 'jwk', key: privateKeyJwk });
+function getPayload(address){
+  // Create the payload for the bootstrap token, note that the
+  // `jsonwebtoken.sign()` method automatically adds the `iat` claim.
+  const payload = {
+    jti: randomUUID(),
+    sub: 'e46b1cb7-9fb5-4e1d-985b-fca1b4e6f217',
+    source: {
+      amount: '100.00',
+      asset: 'USD'
+    },
+    target: {
+      address: '0xb794f5ea0ba39494ce839613fffba74279579268',
+      asset: 'TLOS',
+      network: 'ethereum',
+      label: 'My wallet'
+    }
+  };
+}
+
+
+async function fetchPrivateKey(){
+  // Load private key in JWK format from an environment variable.
+  const privateKeyJwk = JSON.parse(await getKeyBySecretName('topper-widget-key'));
+
+  // Parse the JWK formatted key.
+  const privateKey = createPrivateKey({ format: 'jwk', key: privateKeyJwk });
+  return privateKey;
+}
 
 // Create the options the `jsonwebtoken.sign()` method.
 const options = {
@@ -18,24 +41,9 @@ const options = {
   keyid: '87c793b4-8ba5-4dba-a17d-8e49144d8766'
 };
 
-// Create the payload for the bootstrap token, note that the
-// `jsonwebtoken.sign()` method automatically adds the `iat` claim.
-const payload = {
-  jti: randomUUID(),
-  sub: 'e46b1cb7-9fb5-4e1d-985b-fca1b4e6f217',
-  source: {
-    amount: '100.00',
-    asset: 'USD'
-  },
-  target: {
-    address: '0x0',
-    asset: 'TLOS',
-    network: 'telos',
-    label: 'My wallet'
-  }
-};
 
-async function getBootstrapToken() {
+async function getBootstrapToken(ethAddress) {
+  const privateKey = await fetchPrivateKey();
   const bootstrapToken = await sign(payload, privateKey, options);
   return bootstrapToken;
 }
