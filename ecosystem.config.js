8b163d1884098f1bdb405bb1ba4cfa2c0e930ad8
Viterbo <viter.rod@gmail.com>
2024-12-07 18:18:23 -0300

    version 1.1.4 - ZkEvm faucet

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,6 +9,7 @@ const envShared = {
   testnetFaucetSecondaryIndex: 'AccountName',
   testnetFaucetAccount: 'faucet.tf',
   testnetFaucetKey: 'testnet-faucet-key',
+  zkEvmFaucetKey: 'zkEvm-faucet-key',
   twilioAccountSid: 'tf-twilio-sid',
   twilioAuthToken: 'tf-twilio-auth',
   twilioSmsNumber: '+18178356742',
@@ -45,7 +46,6 @@ const envDev = {
   STLOS_CONTRACT: '0xa9991e4daa44922d00a78b6d986cdf628d46c4dd',
   NETWORK_EVM_RPC: 'https://testnet.telos.net/evm',
   EVM_CHAIN_ID: 41,
-  zkEvmFaucetKey: '751f88a52898aaabc3d534a3b4bb6c2106d4df275d46985e827c335d6ce48a02',
   zkEvmRpcEndpoint: 'http://34.57.215.93:8123',
 };
 
@@ -68,7 +68,6 @@ const envProd = {
   STLOS_CONTRACT: '0xb4b01216a5bc8f1c8a33cd990a1239030e60c905',
   NETWORK_EVM_RPC: 'https://mainnet.telos.net/evm',
   EVM_CHAIN_ID: 40,
-  zkEvmFaucetKey: '751f88a52898aaabc3d534a3b4bb6c2106d4df275d46985e827c335d6ce48a02',
   zkEvmRpcEndpoint: 'http://34.57.215.93:8123',
 };
 
--- src/v1-routes/testnet.js
+++ src/v1-routes/testnet.js
@@ -108,207 +108,206 @@ async function evmFaucetHandler(request, reply) {
 * Similar to evmFaucetOpts, but for the zkEVM network.
 */
 const zkEvmFaucetOpts = {
-  schema: {
-      tags: ['testnet'],
-      params: {
-          type: 'object',
-          properties: {
-              evmAddress: {
-                  description: 'zkEVM address to send ETH to',
-                  type: 'string',
-              },
-          },
-          required: ['evmAddress'],
-      },
-      response: {
-          204: {
-              description: 'Faucet successful',
-              type: 'null',
-          },
-          400: {
-              description: 'Faucet error',
-              type: 'string',
-          },
-      },
-  },
+    schema: {
+        tags: ['testnet'],
+        params: {
+            type: 'object',
+            properties: {
+                evmAddress: {
+                    description: 'zkEVM address to send ETH to',
+                    type: 'string',
+                },
+            },
+            required: ['evmAddress'],
+        },
+        response: {
+            204: {
+                description: 'Faucet successful',
+                type: 'null',
+            },
+            400: {
+                description: 'Faucet error',
+                type: 'string',
+            },
+        },
+    },
 };
 
 async function zkEvmFaucetHandler(request, reply) {
-  try {
-      const ipAddress = request.ips.pop();
-      const actionAllowed = await validateUserAccount(
-          ipAddress,
-          request.params.evmAddress
-      );
-      if (!actionAllowed) {
-          return reply
-              .code(429)
-              .send(
-                  'IP or account has recieved faucet funds within the last 24 hours, please wait and try again'
-              );
-      }
-      await zkEvmFaucet(request.params.evmAddress);
-      reply.code(204);
-  } catch (e) {
-      reply.code(400).send(`Error pouring the faucet: ${e.message}`);
-  }
+    try {
+        const ipAddress = request.ips.pop();
+        const actionAllowed = await validateUserAccount(
+            ipAddress,
+            request.params.evmAddress
+        );
+
+        if (!actionAllowed) {
+            return reply
+                .code(429)
+                .send('IP or account has recieved faucet funds within the last 24 hours, please wait and try again');
+        }
+        await zkEvmFaucet(request.params.evmAddress);
+        reply.code(204);
+    } catch (e) {
+        reply.code(400).send(`Error pouring the faucet: ${e.message}`);
+    }
 }
 
 const accountOpts = {
-  schema: {
-      tags: ['testnet'],
-      body: {
-          required: ['accountName', 'ownerKey', 'activeKey'],
-          type: 'object',
-          properties: {
-              accountName: {
-                  type: 'string',
-                  pattern: '^[a-z1-5]{12}$',
-                  description:
-                      '12 character account name, only characters a-z and 1-5 can be used',
-                  example: 'myaccount123',
-              },
-              ownerKey: {
-                  type: 'string',
-                  pattern: '^EOS[0-9A-Za-z]{50}$',
-                  description: 'Owner public key',
-                  example: 'EOS1234...',
-              },
-              activeKey: {
-                  type: 'string',
-                  pattern: '^EOS[0-9A-Za-z]{50}$',
-                  description: 'Active public key',
-                  example: 'EOS4321...',
-              },
-          },
-      },
-      response: {
-          200: {
-              description: 'Transaction id',
-              type: 'string',
-              example:
-                  '31940262645f44299d1d5d02fb9b313dc9e6b9904677f1410bb63247c9dec234',
-          },
-          400: {
-              description: 'Faucet error',
-              type: 'string',
-              examples: [
-                  {
-                      name: 'Account exists',
-                      value:
-                          'Error creating account: Account ${accountName} already exists',
-                  },
-                  {
-                      name: 'Generic error',
-                      value: 'Error creating account: ${message}',
-                  },
-              ],
-          },
-      },
-  },
+    schema: {
+        tags: ['testnet'],
+        body: {
+            required: ['accountName', 'ownerKey', 'activeKey'],
+            type: 'object',
+            properties: {
+                accountName: {
+                    type: 'string',
+                    pattern: '^[a-z1-5]{12}$',
+                    description:
+                        '12 character account name, only characters a-z and 1-5 can be used',
+                    example: 'myaccount123',
+                },
+                ownerKey: {
+                    type: 'string',
+                    pattern: '^EOS[0-9A-Za-z]{50}$',
+                    description: 'Owner public key',
+                    example: 'EOS1234...',
+                },
+                activeKey: {
+                    type: 'string',
+                    pattern: '^EOS[0-9A-Za-z]{50}$',
+                    description: 'Active public key',
+                    example: 'EOS4321...',
+                },
+            },
+        },
+        response: {
+            200: {
+                description: 'Transaction id',
+                type: 'string',
+                example:
+                    '31940262645f44299d1d5d02fb9b313dc9e6b9904677f1410bb63247c9dec234',
+            },
+            400: {
+                description: 'Faucet error',
+                type: 'string',
+                examples: [
+                    {
+                        name: 'Account exists',
+                        value:
+                            'Error creating account: Account ${accountName} already exists',
+                    },
+                    {
+                        name: 'Generic error',
+                        value: 'Error creating account: ${message}',
+                    },
+                ],
+            },
+        },
+    },
 };
 
 async function accountHandler(request, reply) {
-  try {
-      const { accountName, ownerKey, activeKey } = request.body;
+    try {
+        const { accountName, ownerKey, activeKey } = request.body;
 
-      const accountExistsResult = await accountExists(accountName);
-      if (accountExistsResult) {
-          return reply
-              .code(400)
-              .send(`Error creating account: Account ${accountName} already exists`);
-      }
+        const accountExistsResult = await accountExists(accountName);
+        if (accountExistsResult) {
+            return reply
+                .code(400)
+                .send(`Error creating account: Account ${accountName} already exists`);
+        }
 
-      const ipAddress = request.ips.pop();
-      const actionAllowed = await validateUserAccount(ipAddress);
-      if (!actionAllowed) {
-          return reply
-              .code(429)
-              .send(
-                  'IP or account has received faucet funds within the last 24 hours, please wait and try again'
-              );
-      }
-      const result = await create(accountName, ownerKey, activeKey);
-      reply.send(result.transaction_id);
-  } catch (e) {
-      reply.code(400).send(`Error creating account: ${e.message}`);
-  }
+        const ipAddress = request.ips.pop();
+        const actionAllowed = await validateUserAccount(ipAddress);
+        if (!actionAllowed) {
+            return reply
+                .code(429)
+                .send(
+                    'IP or account has received faucet funds within the last 24 hours, please wait and try again'
+                );
+        }
+        const result = await create(accountName, ownerKey, activeKey);
+        reply.send(result.transaction_id);
+    } catch (e) {
+        reply.code(400).send(`Error creating account: ${e.message}`);
+    }
 }
 
 const addToRotationOpts = {
-  schema: {
-      tags: ['testnet'],
-      params: {
-          type: 'object',
-          properties: {
-              bpAccount: {
-                  description: 'BP account to add to rotation',
-                  type: 'string',
-              },
-          },
-          required: ['bpAccount'],
-      },
-      response: {
-          204: {
-              description: 'Rotation successful',
-              type: 'null',
-          },
-          400: {
-              description: 'Rotation error',
-              type: 'string',
-          },
-      },
-  },
+    schema: {
+        tags: ['testnet'],
+        params: {
+            type: 'object',
+            properties: {
+                bpAccount: {
+                    description: 'BP account to add to rotation',
+                    type: 'string',
+                },
+            },
+            required: ['bpAccount'],
+        },
+        response: {
+            204: {
+                description: 'Rotation successful',
+                type: 'null',
+            },
+            400: {
+                description: 'Rotation error',
+                type: 'string',
+            },
+        },
+    },
 };
 
 async function addToRotationHandler(request, reply) {
-  let result = await rotate(request.params.bpAccount);
-  if (!result.success)
-      reply.code(400).send(`Error trying to rotate: ${result.message}`);
-  else reply.code(204);
+    let result = await rotate(request.params.bpAccount);
+    if (!result.success)
+        reply.code(400).send(`Error trying to rotate: ${result.message}`);
+    else reply.code(204);
 }
 
 const autorotateOpts = {
-  schema: {
-      tags: ['testnet'],
-      response: {
-          204: {
-              description: 'Rotation successful',
-              type: 'null',
-          },
-          400: {
-              description: 'Rotation error',
-              type: 'string',
-          },
-      },
-  },
+    schema: {
+        tags: ['testnet'],
+        response: {
+            204: {
+                description: 'Rotation successful',
+                type: 'null',
+            },
+            400: {
+                description: 'Rotation error',
+                type: 'string',
+            },
+        },
+    },
 };
 
 async function autorotateHandler(request, reply) {
-  const result = await rotate();
-  if (!result.success)
-      reply.code(400).send(`Error trying to rotate: ${result.message}`);
-  else reply.code(204);
+    const result = await rotate();
+    if (!result.success)
+        reply.code(400).send(`Error trying to rotate: ${result.message}`);
+    else reply.code(204);
 }
 
 const getRotationScheduleOpts = {
-  schema: {
-      tags: ['testnet'],
-      response: {
-          200: {
-              description: 'The current rotation schedule',
-              example: ['producer1', 'producer2', 'producer3'],
-              type: 'array',
-              items: {
-                  type: 'string',
-              },
-          },
-      },
-  },
+    schema: {
+        tags: ['testnet'],
+        response: {
+            200: {
+                description: 'The current rotation schedule',
+                example: ['producer1', 'producer2', 'producer3'],
+                type: 'array',
+                items: {
+                    type: 'string',
+                },
+            },
+        },
+    },
 };
 
 async function getRotationScheduleHandler(request, reply) {
-  reply.send(await getLastVoted());
+    reply.send(await getLastVoted());
 }
 
 module.exports = async (fastify, options) => {
